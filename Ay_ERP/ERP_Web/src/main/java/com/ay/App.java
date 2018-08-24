87a85c54c2908e3c3e9f9b87c2f1ea6c4a8c122e
andyli <43850338@qq.com>
2018-08-25 00:59:41 +0800

    rename erp module

---
+++ Ay_ERP/ERP_Web/src/main/java/com/ay/App.java
@@ -0,0 +1,13 @@
+package com.ay;
+
+/**
+ * be used to launch the
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
